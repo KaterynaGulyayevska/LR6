1167c3a212dff5dc9a876d76d8648d5f3e8cfbeb
apiliuk <lohika.artur@98point6.com>
2019-01-15 18:42:05 +0200

    Fix

--- Human.java
+++ Human.java
@@ -1,47 +1,39 @@
-public class Human extends Main {
+public class Human {
+  String name = "Dima";
+  int age = 25;
 
-    String name = "Dima";
-    int age = 25;
+  public String getName() {
+    return name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public void greeting() {
+    System.out.println("Hi! I am a human");
+  }
 
+  public static void howOldYou(int age) {
+    System.out.println(+age);
+  }
 
-   /*
+}
+
+/*
    public Human(String name, int age) {
         this.name = name;
         this.age = age;
     }
     */
-
-    public static void main(String[] args) {
-        Greeting();
-        howOldYou(25);
-        System.out.println("Exit");
-
-    }
-
-    public static void Greeting() {
-        System.out.println("Hi! I am a human");
-    }
-
-
-    public static void howOldYou(int age) {
-        System.out.println(+age);
-    }
 
     /*public int getAge() {
         return age;
@@ -50,7 +42,5 @@ public class Human extends Main {
     public String getName() {
         return name;
     }*/
-}
-
 
 
--- Student.java
+++ Student.java
@@ -1,12 +1,10 @@
-public class Student extends  Human {
+public class Student extends Human {
 
-// Не получается писать сверху метода @Override - выдает ошибку
-    public static void Greeting (){
-
-
-
-            System.out.println("Hi! I am a student");
-            }
+  // Не получается писать сверху метода @Override - выдает ошибку
+  @Override
+  public void greeting() {
+    System.out.println("Hi! I am a student");
+  }
 
 }
 
